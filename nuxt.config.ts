fe882dc4493afc55713b1f3a7311dabeaa90f164
gbayarri <genis.bayarri@irbbarcelona.org>
2024-08-01 12:06:33 +0200

    Updated nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -74,7 +74,8 @@ export default defineNuxtConfig({
   runtimeConfig: {
     mongodbUri: (!process.env.DB_LOGIN || !process.env.DB_PASSWORD) ?
                 `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}` :
-                `mongodb://${process.env.DB_LOGIN}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}?authSource=admin`,
+                //`mongodb://${process.env.DB_LOGIN}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}?authSource=admin`,
+                `mongodb://${process.env.DB_LOGIN}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`,
     public: {
       logoUrl: process.env.CUSTOM === 'true' ? `${baseURL}img/custom_logo.png` : `${baseURL}img/logo.png`,
       apiBase: '/api',
